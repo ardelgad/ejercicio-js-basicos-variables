bb811727fdb559f95d70bd1766cefb44e7bf091a
ardelgad <adelgadoporras@gmail.com>
2022-02-28 14:10:25 +0100

    clase

--- ex3.js
+++ ex3.js
@@ -5,7 +5,7 @@
 let nombre = "Mad";
 let apellido = "Max";
 
-let presentacion = "";
+let presentacion = "Me llamo ";
 
 /**
  * Pregunta 1: ¿Qué valor tengo que asignar a 'presenetacion' para que me muestre pantalla:
@@ -15,10 +15,14 @@ let presentacion = "";
  * 
  */
 
-console.log("Pregunta 1:", presentacion);
+
+console.log("Pregunta 1:", presentacion + nombre + " " + apellido);
 
 /**
  * Pregunta 2: Cambia el valor de las variables adecuadamente para que, en vez de "Me llamo Mad Max", escribe "Me llamo {tu nombre} {tu apellido}"
  */
+let miNombre = "Arantxa"
+let miApellido = "Delgado"
 
-console.log("Pregunta 2:", presentacion);
+
+console.log("Pregunta 2:", presentacion + miNombre + " " + miApellido);
